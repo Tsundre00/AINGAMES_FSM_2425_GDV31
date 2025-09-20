b3ac3794897b93f1de00ff088415b8b4bf2f62c5
202301230Sy <202301230@iacademy.edu.ph>
2025-09-20 11:56:02 +0800

    Added an empty script

---
+++ Assets/Scripts/Changes_Sy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+//change trial_sy
+public class Changes_Sy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
